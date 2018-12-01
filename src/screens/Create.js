c3f263b787ffae3741ff3528802849af98a8fd58
VetaGriff21 <shnuff212@mail.ru>
2018-12-01 16:12:36 +0300

    Add 'CreatorScreen' without db-functions

--- src/screens/Create.js
+++ src/screens/Create.js
@@ -1,34 +1,51 @@
-import React from 'react';
-import {
-  Container, Content, Button, Icon, Left, Text, Header, Right, Body, Title
-} from 'native-base';
+import React, { Component } from "react";
+import { Container, Header, Content, Icon, Item, Left, Body, Button, Right, Title, Picker, Form, Input } from "native-base";
+
 
 export default class CreateScreen extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      selected: undefined,
-      actual: 0,
-      text: ''
+    selectedGame :'',
+    selectedTime: '',
+    text: ''
     };
   }
 
+  onValueChangeGame(value) {
+    this.setState({
+      selectedGame: value
+    });
+  }
+
+  onValueChangeTime(value) {
+    this.setState({
+      selectedTime: value
+    });
+  }
+
   saveAdv() {
-    this.props.navigation.navigate('GameScreen');
+  if (!this.state.selectedTime || !this.state.selectedGame) {
+        alert('Заполните поля');
+    } else {
+    //create and send to db
+    //if text is empty, notification will be without comment
+      this.props.navigation.navigate('GameScreen');
+    }
   }
 
   render() {
     return (
       <Container>
-        <Header>
-          <Left>
-            <Button transparent onPress={() => this.props.navigation.goBack()}>
-              <Icon name='close'/>
-            </Button>
-          </Left>
-          <Body>
+      <Header>
+        <Left>
+          <Button transparent onPress={() => this.props.navigation.goBack()}>
+            <Icon name='close'/>
+          </Button>
+        </Left>
+        <Body>
           <Title>
-            Создание
+            Новая игра
           </Title>
           </Body>
           <Right>
@@ -37,8 +54,45 @@ export default class CreateScreen extends React.Component {
             </Button>
           </Right>
         </Header>
-        <Content padder>
-          <Text>Форма создания</Text>
+        <Content>
+          <Form>
+            <Picker
+              mode="dropdown"
+              iosIcon={<Icon name="ios-arrow-down-outline" />}
+              placeholder="Выберите игру"
+              placeholderStyle={{ color: "#bfc6ea" }}
+              placeholderIconColor="#007aff"
+              style={{ width: undefined }}
+              selectedValue={this.state.selectedGame}
+              onValueChange={this.onValueChangeGame.bind(this)}
+            >
+            <Picker.Item label="Выберите игру" value={0}/>
+            <Picker.Item label="Бильярд" value="Billiards" />
+            <Picker.Item label="Дартс" value="Darts" />
+            <Picker.Item label="Кикер" value="Kicker" />
+            <Picker.Item label="Теннис" value="Tennis" />
+          </Picker>
+          <Picker
+            mode="dropdown"
+            iosIcon={<Icon name="ios-arrow-down-outline" />}
+            placeholder="Актуальность"
+            placeholderStyle={{ color: "#bfc6ea" }}
+            placeholderIconColor="#007aff"
+            style={{ width: undefined }}
+            selectedValue={this.state.selectedTime}
+            onValueChange={this.onValueChangeTime.bind(this)}
+          >
+            <Picker.Item label="Актуальность" value={0} />
+            <Picker.Item label="Бессрочно" value={4} />
+            <Picker.Item label="1 час" value={1} />
+            <Picker.Item label="2 часа" value={2} />
+            <Picker.Item label="3 часа" value={3} />
+          </Picker>
+          <Item>
+            <Icon active name='create' />
+            <Input onChangeText={(text) => this.setState({text: text})} placeholder="Введите комментарий..." />
+          </Item>
+        </Form>
         </Content>
       </Container>
     );
